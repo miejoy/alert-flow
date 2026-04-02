a3a1e4be74873f6d6980c0912add0bd42be95401
Raymond-HuangL <raymond0huang@gmail.com>
2026-04-03 01:00:56 +0800

    chore: 恢复最低平台版本至 iOS 17 / macOS 14

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "alert-flow",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
-        .tvOS(.v16),
-        .watchOS(.v9),
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
